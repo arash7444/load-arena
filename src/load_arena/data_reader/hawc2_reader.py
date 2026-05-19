0c101a2321e2e1cba239b91f98b8b585486f0d1f
Arash <arash.moh62@gmail.com>
2026-05-19 21:37:39 +0200

    first version of HAWC2 reader python file added

--- src/load_arena/data_reader/hawc2_reader.py
+++ src/load_arena/data_reader/hawc2_reader.py
@@ -1,6 +1,20 @@
-from wetb import hawc2
+# import wetb
+import pkgutil
+import inspect
+from Hawc2io import ReadHawc2
+import pandas as pd
 
-test = hawc2
+# help(wetb)
+data = ReadHawc2(
+    r"e:\Projects\Projects\HAWC2_sims\W4000_130\res\dlc13\dlc13_wsp04_wdir000_s023004"
+)
+res_file = data.ReadFLEX()
+sensor_data = data.ChInfo
+print(res_file)
 
+df = pd.DataFrame(res_file, columns=sensor_data[0][1:])
+df.insert(0, "Time", data.t)
 
-print(test)
+print(df.head())
+# for module in pkgutil.iter_modules(wetb.__path__):
+#     print(module.name)
