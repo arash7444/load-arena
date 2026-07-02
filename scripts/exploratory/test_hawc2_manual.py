51987f506bf494a8072fcef00125ffff177aa910
Arash <arash.moh62@gmail.com>
2026-07-02 14:04:59 +0200

    delete useless files in Main branch

--- scripts/exploratory/test_hawc2_manual.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from load_arena.data_reader.hawc2_reader import hawc2_reader
-
-p = Path(
-    r"e:\Projects\Git_Arash\load-arena\tests\h2_res\dlc13\dlc13_wsp04_wdir000_s023004.int"
-)
-df = hawc2_reader(p)
-print("shape:", df.shape)
-print(df.head())
